8437aa5ab3b6a32ccc12aa3e45b7333c32852d3c
anshuravi <anshu.ravi2002@gmail.com>
2022-03-20 17:24:25 -0700

    styling and pictures

--- sketch.js
+++ sketch.js
@@ -19,33 +19,34 @@ let bc = 250;
 let wrds;
 let wds;
 let ws;
+let button;
 
 function preload(){
-  let sword1 = loadImage('sword1.jpg');
-  let sword2 = loadImage('sword2.jpg');
-  let sword3 = loadImage('sword3.jpg');
-  let sword4 = loadImage('sword4.jpg');
-  let sword5 = loadImage('sword5.jpg');
-  let sword6 = loadImage('sword6.jpg');
-  let sword7 = loadImage('sword7.jpg');
-  let sword8 = loadImage('sword8.jpg');
-  let sword9 = loadImage('sword9.jpg');
-  let sword10 = loadImage('sword10.jpg');
-  let sword11 = loadImage('sword11.jpg');
-  let sword12 = loadImage('sword12.jpg');
-  let sword13 = loadImage('sword13.jpg');
-  let sword14 = loadImage('sword14.jpg');
-  let sword15 = loadImage('sword15.jpg');
-  let sword16 = loadImage('sword16.jpg');
-  let sword17 = loadImage('sword17.jpg');
-  let sword18  = loadImage('sword18.jpg');
-  let sword19 = loadImage('sword19.jpg');
-  let sword20 = loadImage('sword20.jpg');
-  let sword21 = loadImage('sword21.jpg');
-  let sword22 = loadImage('sword22.jpg');
-  let sword23 = loadImage('sword23.jpg');
-  let sword24 = loadImage('sword24.jpg');
-  let sword25 = loadImage('sword25.jpg');
+  let sword1 = loadImage('sword1color.png');
+  let sword2 = loadImage('sword2color.png');
+  let sword3 = loadImage('sword3color.png');
+  let sword4 = loadImage('sword4color.png');
+  let sword5 = loadImage('sword5color.png');
+  let sword6 = loadImage('sword6color.png');
+  let sword7 = loadImage('sword7color.png');
+  let sword8 = loadImage('sword8color.png');
+  let sword9 = loadImage('sword9color.png');
+  let sword10 = loadImage('sword10color.png');
+  let sword11 = loadImage('sword11color.png');
+  let sword12 = loadImage('sword12color.png');
+  let sword13 = loadImage('sword13color.png');
+  let sword14 = loadImage('sword14color.png');
+  let sword15 = loadImage('sword15color.png');
+  let sword16 = loadImage('sword16color.png');
+  let sword17 = loadImage('sword17color.png');
+  let sword18 = loadImage('sword18color.png');
+  let sword19 = loadImage('sword19color.png');
+  let sword20 = loadImage('sword20color.png');
+  let sword21 = loadImage('sword21color.png');
+  let sword22 = loadImage('sword22color.png');
+  let sword23 = loadImage('sword23color.png');
+  let sword24 = loadImage('sword24color.png');
+  let sword25 = loadImage('sword25color.png');
   sword =
   [sword1,sword2,sword3,sword4,sword5,sword6,sword7,sword8,sword9,sword10,sword11,sword12,sword13,sword14,sword15,sword16,sword17,sword18,sword19,sword20]
   landscape = loadImage('paper.png');
@@ -53,12 +54,14 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(800, 1000);
+  createCanvas(800, 950);
   background(landscape);
   frameRate(60);
   intText();
   textFont(myFont);
-
+ let button = createButton('Your Destiny Awaits.');
+ button.parent('button-holder');
+ button.mousePressed(buttonPressed);
 
   console.log(mood[int(random(mood.length))]);
   console.log(geo[3])
@@ -71,7 +74,7 @@ function draw() {
   imageMode (CENTER);
 }
 
-function mousePressed() {
+function buttonPressed() {
   imageMode(CORNER);
   background(landscape);
   imageMode (CENTER);
@@ -87,7 +90,7 @@ function mousePressed() {
   fortText();
   console.log(wrds);
   let randoImg = random(sword);
-  image (randoImg, 250, 450, 300,400 );
+  image (randoImg, 250, 560, 300,400 );
 }
 
 function intText() {
@@ -96,16 +99,21 @@ function intText() {
   textSize(20)
   textFont(myFont);
   //this is a lie, you can click anywhere
-  text('Click the page to enter.', width*0.5, height*0.2);
+
 }
 
 function fortText() {
   fill(115, 59, 0)
   intText();
   textAlign(CENTER);
-  text('You meet a', 550, 300);
-  text('in', 550, 400);
-  text('who gave you this sword.', 550, 500);
-  text(wds, 550, 450)
-  text(ws, 550, 350)
+  text('A long time ago, a stone was placed atop a hill by a great sorcerer.', 400, 200);
+  text('A prophet fortold that a sword would be placed within the stone.' , 400, 250);
+  text ('One day whoever pulled out the blade would be ruler of the kingdom.', 400, 300);
+  text('It is now up to you to fulfill this prophecy and claim your destiny.', 400, 350);
+  text('You meet a', 550, 450);
+  text('in', 550, 550);
+  text('who gave you this sword.', 550, 650);
+  text(wds, 550, 600)
+  text(ws, 550, 500)
+  text ('You must place this sword in the stone. Good luck on your journey.', 400, 790);
 }
